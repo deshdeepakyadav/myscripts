4138e33ead260d04b3a0e85613f9caceecb5bf2f
Desh Deepak Yadav <deshdeepak.yadav6@T-mobile.com>
2020-10-08 00:19:18 -0700

    Added Data Typpe

---
+++ dataTypes.js
@@ -0,0 +1,40 @@
+/**Primitive Data Type
+   - Undefined
+   - Null
+   - Boolean
+   - Number
+   - String
+- Complex Data Type
+   - Object
+   - function
+ **/
+
+ // Undefined Data Type
+ var desh;
+ console.log(typeof desh); // undefined
+
+ // Null Data Type
+ var desh = null;
+ console.log(typeof desh); // Object
+
+ // Boolean Data Type
+ var desh = true;
+ console.log(typeof desh); // boolean
+
+ // Number Data type
+ var desh = 7;
+ console.log(typeof desh); // number
+
+ // String Data Type
+ var desh = "deepak";
+ console.log(typeof desh); // string
+
+ // function Data Type
+ var desh = function(){
+     return '';
+ }
+ console.log(typeof desh); // function
+
+ // Object Data Type
+ var desh = { "name": "deepak"}
+ console.log(typeof desh); // object
